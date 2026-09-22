b999f623a16e15585df68ecda7d59311c850cfe5
saivishnu <root@svnu.in>
2026-09-22 16:23:55 +0530

    RegisterActivity layout done

--- app/src/main/java/in/svnu/login_sharedpref/RegisterActivity.java
+++ app/src/main/java/in/svnu/login_sharedpref/RegisterActivity.java
@@ -1,6 +1,8 @@
 package in.svnu.login_sharedpref;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Spinner spCourse, spGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,19 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // get Views
+        spCourse = findViewById(R.id.spCourse);
+        spGender = findViewById(R.id.spGender);
+
+        // course
+        ArrayAdapter<CharSequence> course_adapter = ArrayAdapter.createFromResource(this, R.array.list_course, android.R.layout.simple_spinner_item);
+        course_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spCourse.setAdapter(course_adapter);
+
+        // gender
+        ArrayAdapter<CharSequence> gender_adapter = ArrayAdapter.createFromResource(this, R.array.list_gender, android.R.layout.simple_spinner_item);
+        gender_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spGender.setAdapter(gender_adapter);
     }
 }
