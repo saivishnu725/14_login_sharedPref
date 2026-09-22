c40f6e64c71c86202714ae6616ab744a0ee734ab
saivishnu <root@svnu.in>
2026-09-22 15:58:21 +0530

    main activity part is done

---
+++ README.md
@@ -0,0 +1,17 @@
+# login using Shared Preferences
+
+Shared Preferences and more
+
+## problem statement
+
+create an android application with THREE activities.
+
+First activity should have two buttons such as `Log in` and `Registration`.
+1. by clicking the `Log in` button, it should bring user to the Log in screen
+2. by clicking the `Registration` button, it should bring user to registration page
+3. create a registration page of fields `Username`, `password`, `email`, `phone number`, `course`, `gender`, `age`
+4. apply necessary `Validations` to it:
+    - Make all the fields as mandatory
+    - Password must contain minimum of 6 characters (upper, lower, number). • Phone should accept only 10 numbers
+    - Email should follow the standard format ‘@xyz.COM/EDU/IN/ORG’
+5. use `Shared Preferences` that stores `username` & `password`, and retrieve them for login
--- app/src/main/java/in/svnu/login_sharedpref/LoginActivity.java
+++ app/src/main/java/in/svnu/login_sharedpref/LoginActivity.java
@@ -1,6 +1,5 @@
 package in.svnu.login_sharedpref;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,23 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        // login btn
-        findViewById(R.id.btnLogin).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, LoginActivity.class)));
-
-        // register btn
-        findViewById(R.id.btnRegister).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, RegisterActivity.class)));
     }
 }
